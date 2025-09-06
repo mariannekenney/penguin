fdb175965ff7428819a78be252876c0c340e8a46
Marianne Kenney <39496381+mariannekenney@users.noreply.github.com>
2025-09-06 16:26:33 -0400

    Fix event-details based on event-registration

--- src/event-details/event-details.js
+++ src/event-details/event-details.js
@@ -2,31 +2,13 @@ const labels = Array.from(document.getElementsByClassName('regTypeLiLabel'))
 	.map(element => element.textContent.trim())
 	.filter(label => label !== 'Equipment Only');
 
-const groups = {};
+const generalLabels = labels.filter(label => label.toLowerCase().includes('general'));
 
-labels.forEach(label => {
-	const split = label.split(" ");
-	const key = `${split[0]} ${split[1]}`;
-
-	if (groups[key]) {
-		const newSplit = Number(label.split('$')[1]?.split('.')[0]);
-		const existingSplit = Number(groups[key].split('$')[1]?.split('.')[0]);
-
-		if (newSplit < existingSplit) {
-			groups[key] = label;
-		}
-	} else {
-		groups[key] = label;
-	}
-});
-
-const duplicates = labels.filter(label => label.includes('$') && !Object.values(groups).includes(label));
-
-if (duplicates.length > 0) {
+if (labels.length > generalLabels.length) {
 	document.querySelectorAll('.registrationInfo li').forEach(item => {
 		const label = item.querySelector('.regTypeLiLabel').textContent.trim();
 
-		if (duplicates.includes(label)) {
+		if (generalLabels.includes(label)) {
 			item.style.display = 'none';
 		}
 	})
