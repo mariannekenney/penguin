ffe07d7e5861e1b450285a94a97d8b66e41588eb
Marianne Kenney <39496381+mariannekenney@users.noreply.github.com>
2025-10-10 16:13:34 -0400

    Changes for standardized ticket types

--- src/event-details/event-details.js
+++ src/event-details/event-details.js
@@ -1,15 +1,32 @@
-const labels = Array.from(document.getElementsByClassName('regTypeLiLabel'))
+const today = new Date();
+today.setHours(0, 0, 0, 0);
+const eventDate = new Date(document.querySelector('.eventInfoBoxValue').textContent.trim());
+const diffDays = Math.ceil((eventDate - today) / (1000 * 60 * 60 * 24));
+
+let isEarlyBird;
+if (diffDays >= 10) {
+	isEarlyBird = true;
+} else if (diffDays >= 1 && diffDays <= 9) {
+	isEarlyBird = false;
+}
+
+const hasLTF = Array.from(document.getElementsByClassName('regTypeLiLabel'))
 	.map(element => element.textContent.trim())
-	.filter(label => label !== 'Equipment Only');
+	.some(label => label.includes('LTF'));
 
-const generalLabels = labels.filter(label => label.toLowerCase().includes('general'));
+document.querySelectorAll('.registrationInfo li').forEach(item => {
+	const label = item.querySelector('.regTypeLiLabel').textContent.trim();
 
-if (labels.length > generalLabels.length) {
-	document.querySelectorAll('.registrationInfo li').forEach(item => {
-		const label = item.querySelector('.regTypeLiLabel').textContent.trim();
-
-		if (generalLabels.includes(label)) {
+	if (isEarlyBird == undefined) {
+		item.style.display = 'none';
+	} else if (label !== 'Equipment Only') {
+		if (hasLTF && !label.includes('LTF')) {
 			item.style.display = 'none';
 		}
-	})
-}
+
+		if ((label.includes('Early-Bird') && !isEarlyBird) || (!label.includes('Early-Bird') && isEarlyBird)) {
+			item.style.display = 'none';
+		}
+	}
+});
+
--- src/event-registration/steps/step-two.js
+++ src/event-registration/steps/step-two.js
@@ -1,18 +1,35 @@
 export function execute(x, y, z) {
     // Remove options based on membership / pricing
-    const labels = Array.from(document.querySelectorAll('strong.labelTitle.paymentTitle label'))
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    const eventDate = new Date(document.querySelector('.eventRegistrationInfoEndDate .infoText').textContent.trim().split('-')[0]);
+    eventDate.setHours(0, 0, 0, 0);
+    const diffDays = Math.ceil((eventDate - today) / (1000 * 60 * 60 * 24));
+
+    let isEarlyBird;
+    if (diffDays >= 10) {
+        isEarlyBird = true;
+    } else if (diffDays >= 1 && diffDays <= 9) {
+        isEarlyBird = false;
+    }
+
+    const hasLTF = Array.from(document.querySelectorAll('strong.labelTitle.paymentTitle label'))
         .map(element => element.textContent.trim())
-        .filter(label => label !== 'Equipment Only');
+        .some(label => label.includes('LTF'));
 
-    const generalLabels = labels.filter(label => label.toLowerCase().includes('general'));
+    document.querySelectorAll('.eventRegistrationTypeRadioWrapper').forEach(item => {
+        const label = item.querySelector('label').textContent.trim();
 
-    if (labels.length > generalLabels.length) {
-        document.querySelectorAll('.eventRegistrationTypeRadioWrapper').forEach(item => {
-            const label = item.querySelector('label').textContent.trim();
-
-            if (generalLabels.includes(label)) {
+        if (isEarlyBird == undefined) {
+            item.style.display = 'none';
+        } else if (label !== 'Equipment Only') {
+            if (hasLTF && !label.includes('LTF')) {
                 item.style.display = 'none';
             }
-        })
-    }
+
+            if ((label.includes('Early-Bird') && !isEarlyBird) || (!label.includes('Early-Bird') && isEarlyBird)) {
+                item.style.display = 'none';
+            }
+        }
+    });
 }
